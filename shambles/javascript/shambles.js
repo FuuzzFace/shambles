2b4b4bdf75bdf66b91333fa9cf7cdec54d06ea83
Syed Zeeshan Hasmi <47023273+FuuzzFace@users.noreply.github.com>
2019-02-07 05:21:58 +0530

    My portfolio page only using HTML5 and CSS3,
    My portfolio page only using HTML5 and CSS3, It has some JavaScript Interactivity in it also

---
+++ shambles/javascript/shambles.js
@@ -0,0 +1,86 @@
+var firstChange = document.querySelector("#one");
+    firstChange.addEventListener("click", function(){
+    document.querySelector(".part2 h3").innerHTML = "USER RESEARCH";
+        document.querySelector(".part2 p:nth-of-type(1)").innerHTML = "User research is conducted so as to understand <br> users’ characteristics, aims, and behaviors <br> towards achieving these aims. Its purpose is to <br> produce designs that improve their working<br> practices and lives.";
+        document.querySelector(".part2 p:nth-of-type(2)").innerHTML = "User research also involves the continuous<br> evaluation of the impact of designs on the <br> users, not only during the design and <br> development phase but after long-term use,<br> too";
+});
+var secondChange = document.querySelector("#two");
+    secondChange.addEventListener("click", function(){
+    document.querySelector(".part2 h3").innerHTML = "WIREFRAMING";
+    document.querySelector(".part2 p:nth-of-type(1)").innerHTML = "Wireframing is a way to design a website service <br> at the structural level. A wireframe is commonly<br> used to lay out content and functionality on<br> a page which takes into account user needs<br> and user journeys. Wireframes are used early<br> in the development process to establish the basic<br> structure of a page before visual design and<br> content is added.";
+    document.querySelector(".part2 p:nth-of-type(2)").innerHTML = "A wireframe is a layout of a web page that<br> demonstrates what interface elements will exist on<br> key pages. It is a critical part of the interaction<br> design process.";
+});
+
+var thirdChange = document.querySelector("#three");
+    thirdChange.addEventListener("click", function(){
+    document.querySelector(".part2 h3").innerHTML = "RESPONSIVE DESIGN";
+        document.querySelector(".part2 p:nth-of-type(1)").innerHTML = "Responsive web design (RWD) is an approach to <br>web design that makes web pages render well<br> on a variety of devices and window or<br> screen sizes. ";
+        document.querySelector(".part2 p:nth-of-type(2)").innerHTML = "Recent work also considers the viewer proximity<br> as part of the viewing context as an extension<br> for RWD. Content, design and performance are<br> necessary across all devices to ensure<br> usability and satisfaction";
+                });
+                
+                var fourthChange = document.querySelector("#four");
+    fourthChange.addEventListener("click", function(){
+    document.querySelector(".part2 h3").innerHTML = "ARCHITECTURE";
+        document.querySelector(".part2 p:nth-of-type(1)").innerHTML = "Information architecture(IA) is the structural <br> design of shared information environments; the <br> art and science of organizing and labelling <br> websites, intranets, online communities and <br>software to support usability and findability; and <br> an emerging community of practice focused on <br> bringing principles of design and architecture to <br> the digital landscape.";
+        document.querySelector(".part2 p:nth-of-type(2)").innerHTML = "Typically, it involves a model or concept <br> of information that is used and applied to <br> activities which require explicit details <br> of complex information systems.";
+});
+
+
+
+
+var fifthChange = document.querySelector("#five");
+    fifthChange.addEventListener("click", function () {
+    document.querySelector(".part22 h3").innerHTML = "PHOTOSHOP";
+    document.querySelector(".part22 p:nth-of-type(1)").innerHTML = "Designing good looking clean and <br> functional Web layouts is an <br>essential part of a Web Designers <br>  life.";
+    document.querySelector(".part22 p:nth-of-type(2)").innerHTML = "So taking help from powerful software <br>like PHOTOSHOP is crucial to make<br> amazing cool graphics and content <br> for web pages.";
+});
+
+var sixthChange = document.querySelector("#six");
+    sixthChange.addEventListener("click", function () {
+    document.querySelector(".part22 h3").innerHTML = "PSD TO HTML MAKING";
+    document.querySelector(".part22 p:nth-of-type(1)").innerHTML = "In general, “PSD to HTML” is a workflow. <br>First, a web page is designed in a <br>Photoshop Document(PSD) and then converted to code<br>(using HTML, CSS, and JavaScript).";
+    document.querySelector(".part22 p:nth-of-type(2)").innerHTML = "Then using the slice tool to divide <br> website's imagery and then export it for <br> the web. Write HTML and CSS that <br> utilizes the imagery we exported <br>from Photoshop.";
+});
+                
+var seventhChange = document.querySelector("#seven");
+    seventhChange.addEventListener("click", function () {
+    document.querySelector(".part22 h3").innerHTML = "HTML-5 & CSS-3";
+    document.querySelector(".part22 p:nth-of-type(1)").innerHTML = "These are the latest versions of Hyper <br>Text Markup Language and Cascading <br> Style Sheets to develop the web <br> visual contents";
+    document.querySelector(".part22 p:nth-of-type(2)").innerHTML = "Although these topic are very basic in <br> nature but a good knowledge of it is <br> very helpful in making powerful <br> Web content.";
+});
+
+var eightChange = document.querySelector("#eight");
+    eightChange.addEventListener("click", function () {
+    document.querySelector(".part22 h3").innerHTML = "BOOTSTRAP 3 & 4";
+    document.querySelector(".part22 p:nth-of-type(1)").innerHTML = "Bootstrap 3 & 4 are open source toolkit <br> for developing with HTML, CSS, and JS. ";
+    document.querySelector(".part22 p:nth-of-type(2)").innerHTML = "Quickly prototype your ideas or build <br> your entire app with Sass variables <br> and mixins, responsive grid system, <br> extensive prebuilt components, and <br> powerful plugins built on <br> jQuery. ";
+});
+                        
+    var ninethChange = document.querySelector("#nine");
+    ninethChange.addEventListener("click", function () {
+    document.querySelector(".part22 h3").innerHTML = "JAVASCRIPT INTERACTIVITY";
+    document.querySelector(".part22 p:nth-of-type(1)").innerHTML = "JavaScript is an open source & most <br> popular client side scripting language <br> supported by all browsers.";
+    document.querySelector(".part22 p:nth-of-type(2)").innerHTML = "JavaScript is used mainly for enhancing <br> the interaction of a user with the <br> webpage.";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
